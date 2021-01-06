a03986a175f7220b274c45e203b2441106998b68
Tom Strother <tom_strother@yahoo.com>
2021-01-05 22:35:07 -0600

    Psyc Eval Family page updating to the DB

--- src/components/session/psychologicalEvaluationFamily/PsychologicalEvaluationFamily.js
+++ src/components/session/psychologicalEvaluationFamily/PsychologicalEvaluationFamily.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Label } from "reactstrap";
-import { Form } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
-import Heading from '../../shared/PsychologicalHeading';
-import ButtonNavigation from '../../shared/ButtonNavigation';
-import TextareaAutosize from 'react-textarea-autosize';
-import EmptyFooterSpace from './../../shared/EmptyFooterSpace';
-
+import Heading from "../../shared/PsychologicalHeading";
+import ButtonNavigation from "../../shared/ButtonNavigation";
+import TextareaAutosize from "react-textarea-autosize";
+import EmptyFooterSpace from "./../../shared/EmptyFooterSpace";
+import DataManager from "../../../data_module/DataManager";
+import convertID from "../../../helpers/formFieldIdConverter";
 
 function PsychologicalEvaluation_family(props) {
-
+  const [item, setItem] = useState("");
   const [patientParents, setPatientParents] = useState({
     patient_father_first_name: "",
     patient_father_last_name: "",
@@ -18,132 +24,216 @@ function PsychologicalEvaluation_family(props) {
     patient_guardian_first_name: "",
     patient_guardian_last_name: "",
     patient_guardian_gender: "",
-  })
+  });
 
   const next = "/psychological_evaluation_siblings";
 
+  const [dropdownOpen1, setDropdownOpen1] = useState(false);
+  const toggle1 = () => setDropdownOpen1((prevState) => !prevState);
+
+
+  const convertIDfunc = (e) => {
+    const fieldID = convertID.convertID(e);
+    setItem(fieldID);
+  };
+
   const handleFieldChange = (e) => {
-    setPatientParents({ ...patientParents, [e.target.name]: e.target.value});
-  }
+    setPatientParents({ ...patientParents, [e.target.name]: e.target.value });
+  };
+
+  const updatePatient = () => {
+    const editedPatient = {
+      id: props.userId,
+      patient_father_first_name: patientParents.patient_father_first_name,
+      patient_father_last_name: patientParents.patient_father_last_name,
+      patient_mother_first_name: patientParents.patient_mother_first_name,
+      patient_mother_last_name: patientParents.patient_mother_last_name,
+      patient_guardian_first_name: patientParents.patient_guardian_first_name,
+      patient_guardian_last_name: patientParents.patient_guardian_last_name,
+      patient_guardian_gender: patientParents.patient_guardian_gender,
+    };
+
+    DataManager.update("patients", editedPatient).then(() => {
+      console.log(editedPatient);
+    });
+  };
+
+  //CRUD Function END
+
+  useEffect(() => {
+    DataManager.getPatient(props.userId).then((patientInfo) => {
+      setPatientParents(patientInfo);
+    });
+  }, []);
 
   return (
     <>
-     <div id="page-container">
-    <div id="content-wrap">
-      <Heading />   
-      <div className="minWidthContainer">  
-      <div className="header">
-        <h1 className="textWhite mt-2">Please Confirm or Update Information</h1>
-      </div>
-      <div className="row no-gutters d-flex justify-content-center align-content-center">
-          <div className="col-6">
-          <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for="firstName">
-              Parents
-            </Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_father_first_name"
-              name="patient_father_first_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_father_first_name}
-              placeholder="Father First Name"
-            />
-          </div>
-          <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for=""></Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_father_last_name"
-              name="patient_father_last_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_father_last_name}
-              placeholder="Father Last Name"
-            />
-          </div>
-          <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for=""></Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_mother_first_name"
-              name="patient_mother_first_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_mother_first_name}
-              placeholder="Mother First Name"
-            />
+      <div id="page-container">
+        <div id="content-wrap">
+          <Heading />
+          <div className="minWidthContainer">
+            <div className="header">
+              <h1 className="textWhite mt-2">
+                Please Confirm or Update Information
+              </h1>
             </div>
-             <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for=""></Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_mother_last_name"
-              name="patient_mother_last_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_mother_last_name}
-              placeholder="Mother Last Name"
-            />
-          </div>
-            </div>
-     
-{/* --------------------------------------------- */}    
-<div className="col-6">
-          <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for="caseNumber">
-              Other Guardians
-            </Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_guardian_first_name"
-              name="patient_guardian_first_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_guardian_first_name}
-              placeholder="Guardian First Name"
-            />
-          </div>
-          <div className="d-flex m-4">
-            <Label className="textWhite labelWidth" for="examplePassword"></Label>
-            <TextareaAutosize              
-              className="fieldData col-8"
-              type="text"
-              id="patient_guardian_last_name"
-              name="patient_guardian_last_name"
-              onChange={handleFieldChange}
-              value={patientParents.patient_guardian_last_name}
-              placeholder="Guardian Last Name"
-              />
-          </div>
-          <div className="d-flex m-4 flex-wrap">
-          <Form.Label className="textWhite m-1 col-2 text-right">Gender: </Form.Label>
-          <Form.Control 
-          as="select" 
-          className="col-3" 
-          defaultValue={patientParents.patient_guardian_gender}
-          onChange={handleFieldChange}
-          >
-            <option>Please Select</option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Unspecified">Unspecified</option>
-          </Form.Control>
-          </div>
-          <div className="text-center">
-            <div className="textWhite"><i className="fas fa-plus fa-lg mr-2"></i>Click to Add More Guardians</div>
-          </div>
-        </div>
-        </div>
-        </div>
-      <div id="footer"> 
-      <ButtonNavigation next={next} />
-      <EmptyFooterSpace />
-      </div>
-      </div>
-      </div>
+            <div className="row no-gutters d-flex justify-content-center align-content-center">
+              <div className="col-6">
+                <div className="d-flex m-4">
+                  <Label className="textWhite labelWidth" for="firstName">
+                    Parents
+                  </Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_father_first_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_father_first_name}
+                    placeholder="Father First Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+                <div className="d-flex m-4">
+                  <Label className="textWhite labelWidth" for=""></Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_father_last_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_father_last_name}
+                    placeholder="Father Last Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+                <div className="d-flex m-4">
+                  <Label className="textWhite labelWidth" for=""></Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_mother_first_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_mother_first_name}
+                    placeholder="Mother First Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+                <div className="d-flex m-4">
+                  <Label className="textWhite labelWidth" for=""></Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_mother_last_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_mother_last_name}
+                    placeholder="Mother Last Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+              </div>
 
+              {/* --------------------------------------------- */}
+              <div className="col-6">
+                <div className="d-flex m-4">
+                  <Label className="textWhite labelWidth" for="caseNumber">
+                    Other Guardians
+                  </Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_guardian_first_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_guardian_first_name}
+                    placeholder="Guardian First Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+                <div className="d-flex m-4">
+                  <Label
+                    className="textWhite labelWidth"
+                    for="examplePassword"
+                  ></Label>
+                  <TextareaAutosize
+                    className="fieldData col-8"
+                    type="text"
+                    id={item}
+                    name="patient_guardian_last_name"
+                    onChange={handleFieldChange}
+                    value={patientParents.patient_guardian_last_name}
+                    placeholder="Guardian Last Name"
+                    onClick={convertIDfunc}
+                  />
+                </div>
+                <div className="d-flex m-4 flex-wrap">
+                  <Dropdown
+                    isOpen={dropdownOpen1}
+                    toggle={toggle1}
+                    className="col-8"
+                  >
+                    <DropdownToggle
+                      color="light"
+                      className="dropdown text-center"
+                      caret
+                      value={patientParents.patient_guardian_gender}
+                    >
+                      {patientParents.patient_guardian_gender}
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem
+                        onClick={handleFieldChange}
+                        name="patient_guardian_gender"
+                        value="None Selected"
+                      >
+                        None Selected
+                      </DropdownItem>
+                      <DropdownItem
+                        onClick={handleFieldChange}
+                        name="patient_guardian_gender"
+                        value="Female"
+                      >
+                        Female
+                      </DropdownItem>
+                      <DropdownItem
+                        onClick={handleFieldChange}
+                        name="patient_guardian_gender"
+                        value="Male"
+                      >
+                        Male
+                      </DropdownItem>
+                      <DropdownItem
+                        onClick={handleFieldChange}
+                        name="patient_guardian_gender"
+                        value="Other"
+                      >
+                        Other
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </div>
+                <div className="text-center">
+                  <div className="textWhite">
+                    <i className="fas fa-plus fa-lg mr-2"></i>Click to Add More
+                    Guardians
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="footer">
+            <ButtonNavigation
+              next={next}
+              updatePatient={updatePatient}
+              patient={props.userId}
+              patientNotes={patientParents}
+            />
+            <EmptyFooterSpace />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
