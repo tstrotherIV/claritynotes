ea7f1ad5fd2bfaefbaed21dd898add119b178e46
Tom Strother <tom_strother@yahoo.com>
2021-01-20 20:19:16 -0600

    added a toggle to create an edit field for the paragraph

--- src/components/session/referral/referral.js
+++ src/components/session/referral/referral.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import EmptyFooterSpace from "../../shared/EmptyFooterSpace";
 import "./referral.scss";
+import { Input, Button } from "reactstrap";
 import ButtonNavigation from "../../shared/ButtonNavigation";
 import DataManager from "../../../data_module/DataManager";
 import { AgeFromDateString } from "age-calculator";
+import TextareaAutosize from "react-textarea-autosize";
 
 function PsychologicalEvaluationReferral(props) {
   const [patient, setPatient] = useState("");
   const [patientChildren, setPatientChildren] = useState([]);
   const [lastItem, setLastItem] = useState([]);
   const [startItem, setStartItem] = useState([]);
+  const [paragraphEnableEdit, setParagraphEnableEdit] = useState(false);
+  const [paragraph, setParagraph] = useState("");
+  const [modifiedParagraph, setModifiedParagraph] = useState("");
 
   const next = "/psychological_evaluation_additional_data";
   const back = "/psychological_evaluation_consent";
@@ -27,6 +32,47 @@ function PsychologicalEvaluationReferral(props) {
     }
   };
 
+  const handleCheckBoxChange = (e) => {
+    const target = e.target;
+    const value = target.checked;
+    const name = target.name;
+    getParagraph();
+    setParagraphEnableEdit({ ...paragraphEnableEdit, [name]: value });
+  };
+
+  const DefaultParagraph = () => {
+    return (
+      <div className="centerText" id="referral_paragraph">
+        {String(patient.patient_first_name)} {String(patient.patient_last_name)}{" "}
+        is a {String(ageFromString)} year old {String(patient.patient_married)}{" "}
+        {String(patient.patient_gender)} who was referred by the [Referral] for
+        a psychological evaluation to determine {String(patientProNoun())}{" "}
+        current cognitive, emotional, behavioral, and psychological status and
+        make recommendations as to {String(patientProNoun())} ability to
+        protect, provide for, and care for {String(patientProNoun())} children{" "}
+        {String(
+          startItem.map((child) => {
+            let childAge = new AgeFromDateString(child.child_dob).age;
+            return ` ${child.child_first_name} (${childAge}), `;
+          })
+        )}{" "}
+        and{" "}
+        {String(
+          lastItem.map((child) => {
+            let childAge = new AgeFromDateString(child.child_dob).age;
+            return ` ${child.child_first_name} (${childAge}) `;
+          })
+        )}
+        and maintain their placement in {String(patientProNoun())} home.
+      </div>
+    );
+  };
+
+  const getParagraph = () => {
+    const need = document.getElementById("referral_paragraph").innerHTML;
+    setParagraph(String(need));
+  };
+
   const getData = () => {
     DataManager.getPatient(props.patientId).then((patientInfo) => {
       setPatient(patientInfo);
@@ -39,6 +85,24 @@ function PsychologicalEvaluationReferral(props) {
       setStartItem(children.slice(0, -1));
       setLastItem(children.slice(-1));
     });
+  };
+
+  const handleChange = (e) => {
+    const editedParagraph = {
+      ...paragraph,
+      [e.target.name]: e.target.value,
+    }
+    DataManager.update("patientNotes", editedParagraph);
+  }
+
+  const updatePatient = () => {
+    const editedPatient = {
+      id: props.patientId,
+      referral_paragraph_modified:
+        modifiedParagraph.referral_paragraph_modified,
+    };
+
+    DataManager.update("patients", editedPatient).then(() => {});
   };
 
   useEffect(() => {
@@ -58,34 +122,49 @@ function PsychologicalEvaluationReferral(props) {
               <div className="d-flex justify-content-center mt-5">
                 <h2>Reason for Referral</h2>
               </div>
+
               <div className="d-flex justify-content-center mt-5">
-                <div className="centerText">
-                  {patient.patient_first_name} {patient.patient_last_name} is a{" "}
-                  {ageFromString} year old {patient.patient_married}{" "}
-                  {patient.patient_gender} who was referred by the [Referral]
-                  for a psychological evaluation to determine {patientProNoun()}{" "}
-                  current cognitive, emotional, behavioral, and psychological
-                  status and make recommendations as to {patientProNoun()}{" "}
-                  ability to protect, provide for, and care for{" "}
-                  {patientProNoun()} children{" "}
-                  {startItem.map((child) => {
-                    let childAge = new AgeFromDateString(child.child_dob).age;
-                    return ` ${child.child_first_name} (${childAge}), `;
-                  })}{" "}
-                  and{" "}
-                  {lastItem.map((child) => {
-                    let childAge = new AgeFromDateString(child.child_dob).age;
-                    return ` ${child.child_first_name} (${childAge}) `;
-                  })}
-                  and maintain their placement in {patientProNoun()} home.
-                </div>
+                {!paragraphEnableEdit ? (
+                  <>
+                    <div className="m-4">
+                      <Input
+                        type="checkbox"
+                        id="referral_paragraph_enable"
+                        name="referral_paragraph_enable"
+                        onChange={handleCheckBoxChange}
+                      />
+                    </div>
+                    {patient.referral_paragraph_modified ? (
+                      <div>{patient.referral_paragraph_modified}</div>
+                    ) : (
+                      <DefaultParagraph />
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <TextareaAutosize
+                      className="interview_fieldData"
+                      type="text"
+                      name="referral_paragraph_modified"
+                      id="referral_paragraph_modified"
+                      value={paragraph}
+                      // onChange={handleChange}
+                    />
+                    <Button>Save Changes</Button>
+                  </>
+                )}
               </div>
             </div>
           </div>
         </div>
       </div>
       <div id="footer">
-        <ButtonNavigation next={next} back={back} patient={props.patientId} />
+        <ButtonNavigation
+          next={next}
+          back={back}
+          patient={props.patientId}
+          updatePatient={updatePatient}
+        />
         <EmptyFooterSpace />
       </div>
     </div>
